2d38626b80eb96f027847551a3f98f91647017d3
SamirSalimov <samir.salimovv@gmail.com>
2017-08-18 01:23:33 +0300

    added index column to dataframe ,so now this is how columns looks 'Index', 'Open', 'High', 'Low', 'Close',  'Volume']

--- candlestick_patterns/pull_data.py
+++ candlestick_patterns/pull_data.py
@@ -9,13 +9,14 @@ class ImportDataFrame():
     year = None
     end = dt.datetime.today()
 
-    #TODO - add index column to dataframe
-
     def __init__(self, stock_id):
         self.stock_name = stock_id
         # create a DATA FRAME  with values from google finance  from year 2000 till today
         self.il = web.DataReader(self.stock_name, 'google', self.start, self.end)
-
+        # adding Index column evaluated bu row number , starting from 0
+        self.il = self.il.assign( Index= [i for i in  range(len(self.il))])
+        self.il.set_index('Index', inplace=True)
+     #   self.il = self.il[['Index', 'Open', 'High', 'Low', 'Close',  'Volume']]
     # this function returns data_frame with data from last year only
     def get_last_year(self):
         print(self.il.last_valid_index())
